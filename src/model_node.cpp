623bbc478d16a236003b9cb57c56d24e64fbc5b5
user <skull8888888@gmail.com>
2020-10-05 17:24:15 +0900

    verified that c++ and python model output the same

--- src/model_node.cpp
+++ src/model_node.cpp
@@ -51,6 +51,7 @@ public:
     try {
         // Deserialize the ScriptModule from a file using torch::jit::load().
       module = torch::jit::load("/home/user/catkin_ws/src/f1t/jn/balanced.pt");
+      module.eval();
     }
     catch (const c10::Error& e) {
       std::cerr << e.what();
@@ -62,9 +63,19 @@ public:
 
     // Execute the model and turn its output into a tensor.
     // std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+    
+    cv::namedWindow(OPENCV_WINDOW);
+    cv::startWindowThread();  
+
+    // cv::Mat img = cv::imread("/home/user/catkin_ws/images/test/left0000.jpg");
+    // model_forward(img);
 
   }
 
+  ~ModelNode()
+  {
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
 
   void imageCb(const sensor_msgs::CompressedImageConstPtr& msg)
   {
@@ -80,10 +91,20 @@ public:
       return;
     }
 
-    cv::Mat img;
-    cv::cvtColor(cv_ptr->image, img, CV_RGB2GRAY);
-    cv::cvtColor(img, img, CV_GRAY2RGB);
+    cv::Mat img = cv_ptr->image;
+    model_forward(img);
+    // sensor_msgs::ImagePtr out_msg = cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::MONO8, img).toImageMsg();
+    // image_pub_.publish(out_msg);   
 
+    //////////////////////////////////////////////////
+    
+  }
+
+  void model_forward(cv::Mat& img){
+
+    cv::cvtColor(img, img, CV_RGB2GRAY);
+    cv::cvtColor(img, img, CV_GRAY2RGB);
+    
     cv::Rect roi;
 
     int offset_x = 0;
@@ -94,28 +115,45 @@ public:
     roi.width = img.size().width;
     roi.height = img.size().height - offset_y;
 
-    // 640x240 cut 
-    // 640x480 original
+    // 480x640 original
+    // 240x640 cut 
     // ROS_INFO("%d %d", roi.width, roi.height);
     img = img(roi);
     cv::resize(img, img, cv::Size(320,120));
+    // ROS_INFO("%u %u", img.rows, img.cols);  
+    imshow(OPENCV_WINDOW, img);
+    
+    // normalizing image to fit torch tensor input format
+    img.convertTo(img, CV_32FC3, 1.0/255.0);
+    // imshow(OPENCV_WINDOW, img);
+    // std::cout << img << std::endl;
 
+    auto tensor_image = torch::from_blob(img.data, {1, img.rows, img.cols, img.channels()});
+    tensor_image = tensor_image.permute({0,3,1,2});
     
-    auto tensor_image = torch::from_blob(img.data, {1, img.rows, img.cols, img.channels() }, at::kByte);
-    tensor_image = tensor_image.permute({0,3,1,2}).to(at::kFloat);
-    
+    // std::cout << tensor_image << std::endl;
+
     std::vector<torch::jit::IValue> inputs;
     torch::Device device(torch::kCUDA);
     inputs.push_back(tensor_image.to(device));
 
-    at::Tensor output = module.forward(inputs).toTensor();
-    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
-
-    // sensor_msgs::ImagePtr out_msg = cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::MONO8, img).toImageMsg();
-    // image_pub_.publish(out_msg);   
-
-    //////////////////////////////////////////////////
+    // auto out_tensor = tensor_image;
+    // auto s = out_tensor.sizes();
     
+    // out_tensor = out_tensor.squeeze().permute({2, 1, 0});
+    
+    // // ROS_INFO("%u %u %u %u", s[0], s[1], s[2], s[3]);
+    // out_tensor = out_tensor.to(torch::kU8);
+    // out_tensor = out_tensor.to(torch::kCPU);
+
+    // cv::Mat resultImg(120, 320, CV_8UC3);
+    // std::memcpy((void *) resultImg.data, out_tensor.data_ptr(), sizeof(torch::kU8) * out_tensor.numel());
+
+    // imshow(OPENCV_WINDOW, resultImg);
+    
+    at::Tensor output = module.forward(inputs).toTensor();
+    std::cout << output[0][0].item<float>() << '\n';
+
   }
 
 };
